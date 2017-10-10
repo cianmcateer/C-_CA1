2b990dc8da50d8b041f5489d46653709b0a21281
cianmcateer <cianmcateer@gmail.com>
2017-10-10 19:05:47 +0100

    init function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 #include <string>
 
+using std::string;
 using std::cout;
 using std::cin;
 using std::endl;
+
+void init();
+bool login();
+
+int main(void) {
+
+    init();
+    return 0;
+}
+
+void init() {
+    cout << "Welcome to e-school.com" << endl;
+    
+
+}
