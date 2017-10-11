c31bec7e151b9da090d54246456f92228125c55f
cianmcateer <cianmcateer@gmail.com>
2017-10-11 19:50:42 +0100

    First iteration of Student header

---
+++ Student.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <ostream>
+
+
+using std::string;
+using std::map;
+using std::cout;
+using std::endl;
+using std::vector;
+
+class Student {
+private:
+    string name;
+    int age;
+    float gpa;
+    int attendance;
+    string comment;
+
+public:
+    Student(string name, int age, int attendance, float gpa, string comment);
+    ~Student();
+    bool has_passed();
+
+    inline string get_name() {
+        return name;
+    }
+
+    inline int get_age() {
+        return age;
+    }
+
+    inline int get_attendance() {
+        return attendance;
+    }
+
+    inline float get_gpa() {
+        return gpa;
+    }
+
+    inline string get_comment() {
+        return comment;
+    }
+
+    inline void set_name(string name) {
+        this->name = name;
+    }
+
+    inline void set_age(int age) {
+        this->age = age;
+    }
+
+    inline void set_attendance(int attendance) {
+        this->attendance = attendance;
+    }
+
+    inline void set_gpa(float gpa) {
+        this->gpa = gpa;
+    }
+
+    inline void set_comment(string comment) {
+        this->comment = comment;
+    }
+
+};
+
+inline std::ostream& operator<<(std::ostream& output_stream, Student& s) {
+    output_stream << "NAME: " << s.get_name() << " AGE: " << s.get_age()
+    << " ATTENDANCE: " << s.get_attendance() << " GPA: " << s.get_gpa()
+    << " COMMENT: " << s.get_comment() << endl;
+
+    return output_stream;
+}
