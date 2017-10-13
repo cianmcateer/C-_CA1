6f50c555342446bb084dff4eef201453eb3e1aed
cianmcateer <cianmcateer@gmail.com>
2017-10-13 12:19:40 +0100

    Menu made

--- main.cpp
+++ main.cpp
@@ -2,8 +2,12 @@
 #include <string>
 #include <fstream>
 
+#include "Student.h"
+#include "Student_Store.h"
+
 using std::string;
 using std::cout;
+using std::cin;
 using std::endl;
 
 
@@ -14,7 +18,7 @@ bool login();
 bool is_password(string password);
 
 int main(void) {
-
+    Student_Store s;
     init();
     return 0;
 }
@@ -35,6 +39,7 @@ void init() {
     int attempt = 4;
     while(!is_password(password_attempt)) {
         if(attempt < 1) {
+            cout << "Access Denied, system has shut down!" << endl;
             exit(0);
         }
         --attempt;
@@ -50,8 +55,9 @@ void init() {
 }
 
 void read_file() {
-    const string path = "/Users/CianMcAteer/Desktop/year_3/semester_1/C++/C++_CA1/files/help.txt";
-    std::ifstream help_menu(path);
+    // Fix file path problem
+    const string help_path = "/Users/CianMcAteer/Desktop/year_3/semester_1/C++/C++_CA1/files/help.txt";
+    std::ifstream help_menu(help_path);
     string line;
     if(help_menu.is_open()) {
         // Will read line until new line is detected
@@ -66,6 +72,44 @@ void read_file() {
 
 void menu() {
     read_file();
+
+    int choice;
+    Student_Store st;
+    while(true) {
+        cin >> choice;
+
+        switch(choice) {
+            case 0:
+                cout << "Goodbye!" << endl;
+                exit(0);
+                break;
+
+            case 1:
+                st.print();
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            case 4:
+                break;
+
+            case 5:
+                break;
+
+            case 6:
+                break;
+
+            case 7:
+                break;
+
+            case 8:
+                break;
+        }
+    }
 }
 
 bool is_password(string password) {
