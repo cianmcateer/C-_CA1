417b7279f96be19d5078cff7487187332a27df77
cianmcateer <cianmcateer@gmail.com>
2017-10-18 22:57:33 +0100

    Got rid of namespaces

--- Student_Store.cpp
+++ Student_Store.cpp
@@ -7,12 +7,12 @@ Student_Store::Student_Store() {
 
 Student_Store::~Student_Store() {}
 
-void Student_Store::load(map<string,vector<Student> >& school_data) {
+void Student_Store::load(std::map<std::string,std::vector<Student> >& school_data) {
 
     Student s1("CianMcAteer",21,15,44,"Good");
     Student s2("Jp mcdoug",15,77,44,"shite");
     Student s3("sam sung",44,11,33,"grand");
-    vector<Student> vec1;
+    std::vector<Student> vec1;
     vec1.push_back(s1);
     vec1.push_back(s2);
     vec1.push_back(s3);
@@ -20,7 +20,7 @@ void Student_Store::load(map<string,vector<Student> >& school_data) {
     Student s4("Jamie samuel",22,13,22,"Excellent");
     Student s5("micky samuel",12,23,52,"good effort");
     Student s6("Jamie jones",11,12,11,"Wonderful");
-    vector<Student> vec2;
+    std::vector<Student> vec2;
     vec2.push_back(s4);
     vec2.push_back(s5);
     vec2.push_back(s6);
@@ -30,14 +30,7 @@ void Student_Store::load(map<string,vector<Student> >& school_data) {
 
 }
 
-bool Student_Store::is_full(string& teacher) {
-    if(school_data[teacher].size() > 30) {
-        return true;
-    }
-    return false;
-}
-
-void Student_Store::add(string& teacher, const Student& s) {
+void Student_Store::add(std::string& teacher, const Student& s) {
     school_data[teacher].push_back(s);
 }
 
@@ -45,18 +38,31 @@ void Student_Store::clear() {
     school_data.clear();
 }
 
+void Student_Store::update(std::string& teacher, std::string& name, int& age, int& attendance, int& gpa, std::string& comment) {
+
+}
+
+
+
+bool Student_Store::is_full(std::string& teacher) {
+    if(school_data[teacher].size() > 30) {
+        return true;
+    }
+    return false;
+}
+
 void Student_Store::print() {
     for(auto& sd : school_data) {
-        cout << sd.first << ":" << endl;
+        std::cout << sd.first << ":" << std::endl;
         for(auto& s : sd.second) {
-            cout << "\t" << s << endl;
+            std::cout << "\t" << s << std::endl;
         }
     }
 }
 
 
 
-map<string,vector<Student> > Student_Store::read_file() {
+std::map<std::string,std::vector<Student> > Student_Store::read_file() {
 /*    const string file_path = "data.txt";
     std::ifstream read_file(file_path);
 
@@ -88,37 +94,40 @@ map<string,vector<Student> > Student_Store::read_file() {
     return file_data;*/
 }
 
-void Student_Store::update(string& teacher, string& name, int& age, int& attendance, int& gpa, string& comment) {
-
-
-
-}
-
-void Student_Store::display_group(string& teacher) {
+void Student_Store::display_group(std::string& teacher) {
     if(school_data[teacher].empty()) {
-        cout << "No students have been assigned to "
-        << teacher << " yet" << endl;
+        std::cout << "No students have been assigned to "
+        << teacher << " yet" << std::endl;
     } else {
         for(auto& s : school_data[teacher]) {
-            cout << s << endl;
+            std::cout << s << std::endl;
         }
     }
 }
 
+void create_group(std::string& teacher) {
+//    vector<Student> new_group;
+//    school_data.insert(teacher,new_group);
+}
+
+void remove(const std::string& teacher) {
+    school_data.erase(teacher);
+}
+
 void Student_Store::save() {
-    const string file_path = "data.txt";
+    const std::string file_path = "data.txt";
     std::ofstream save_file(file_path);
     if(save_file.is_open()) {
         for(auto& sd : school_data) {
-            save_file << sd.first << "," << sd.second.size() << endl;
+            save_file << sd.first << "," << sd.second.size() << std::endl;
             for(auto& s : sd.second) {
                 save_file << s.get_name() << "," << s.get_age() <<
                 "," << s.get_gpa() << "," << s.get_attendance() <<
-                s.get_comment() << endl;
+                s.get_comment() << std::endl;
             }
         }
     } else {
-        std::cerr << "File not open" << endl;
+        std::cerr << "File not open" << std::endl;
     }
 
 }
