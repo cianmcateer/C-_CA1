7691fc9a17157e14c73ea8fccaf4899b0b24306a
cianmcateer <cianmcateer@gmail.com>
2017-10-28 12:49:43 +0100

    Read file complete

--- Student_Store.h
+++ Student_Store.h
@@ -6,17 +6,21 @@
 #include <string>
 #include <fstream>
 #include <algorithm>
+#include <sstream>
+#include <cstring>
 
 #include "Student.h"
 
 
-class Student_Store{
+class Student_Store {
 
 private:
     // Local storage
     std::map<std::string,std::vector<Student> > school_data;
     std::map<std::string,std::vector<Student> > read_file();
     void load(std::map<std::string,std::vector<Student> >& school_data);
+    void replace_characters(Student& s, char old_char, char new_char);
+    std::string save_path;
 
 public:
     // Constructor / Destructor
@@ -25,16 +29,16 @@ public:
 
     void add(std::string& teacher, const Student& s);  // Basic implementation finished
     void clear(); // Done
-    void update(std::string& teacher ,std::string& name, int& age, int& attendance, int& gpa, std::string& comment);
-    void remove(const std::string& teacher);
+    void update(std::string& teacher ,std::string& name, int& age, int& attendance, int& gpa, std::string& comment); // Not implemented
+    void remove_group(const std::string& teacher); // Not implemented
 
-    void print();
-    void display_group(std::string& teacher);
+    void print(); // Need to sort
+    void display_group(std::string& teacher); //
     void create_group(std::string& teacher);
 
-    void save();
+    void save(); // Done
 
-    bool is_full(std::string& teacher);
+    bool is_full(std::string& teacher); // Done not implemented yet in main
 
 
 };
