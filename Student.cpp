bd597e4bd15b0b4ee2262f0ffcef65d55626d51b
cianmcateer <cianmcateer@gmail.com>
2017-10-28 19:30:59 +0100

    Test

--- Student.cpp
+++ Student.cpp
@@ -6,7 +6,13 @@ Student::Student(std::string name, int age, int attendance, float gpa ,std::stri
 
 }
 
-Student::Student(){}
+Student::Student(){
+    this->name = "Default name";
+    this->age = 0;
+    this->attendance = 0;
+    this->gpa = 0;
+    this->comment = "Default comment";
+}
 
 Student::~Student(){}
 
