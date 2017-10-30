43cfb32e5b165f708f70ca166c0eb58309fe4be1
cianmcateer <cianmcateer@gmail.com>
2017-10-30 22:43:37 +0000

    'passed' variable added, not functioning properly as of yet

--- Student.cpp
+++ Student.cpp
@@ -1,9 +1,8 @@
 #include "Student.h"
 
-
 Student::Student(std::string name, int age, int attendance, float gpa ,std::string comment)
 : name(name) , age(age) , attendance(attendance), gpa(gpa), comment(comment)  {
-
+    this->passed = this->gpa >= 40;
 }
 
 Student::Student(){
@@ -11,14 +10,11 @@ Student::Student(){
     this->age = 0;
     this->attendance = 0;
     this->gpa = 0;
+    this->passed = false;
     this->comment = "Default comment";
 }
 
 Student::~Student(){}
-
-bool Student::has_passed()  {
-    return gpa >= 40;
-}
 
 std::string Student::to_html() {
 
