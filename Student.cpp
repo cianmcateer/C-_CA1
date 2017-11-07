2919a48e3cc0396a422633adc18a7b2c4dc3abe2
cianmcateer <cianmcateer@gmail.com>
2017-11-07 10:27:22 +0000

    Overloaded operators moved from .h file

--- Student.cpp
+++ Student.cpp
@@ -9,6 +9,8 @@ Student::~Student(){}
 
 std::string Student::to_html() {
 
+    // ostringstream is used instead as we
+    // don't need to convert floats and ints to strings
     std::ostringstream oss;
 
     oss << "<td>" << this->name << "</td>"
@@ -18,4 +20,52 @@ std::string Student::to_html() {
     << "<td>" << this->comment << "</td>";
 
     return oss.str();
+}
+
+std::ostream& operator<<(std::ostream& output_stream, const Student& s) {
+    output_stream << "NAME: " << s.get_name() << " AGE: " <<
+    s.get_age() << " ATTENDANCE: " <<
+    s.get_attendance() << " GPA: " << s.get_gpa() << " COMMENT: " << s.get_comment() << std::endl;
+
+    return output_stream;
+}
+
+std::istream& operator>>(std::istream& input_stream, Student& s) {
+
+    std::string name;
+    input_stream >> name;
+    s.set_name(name);
+
+    int age;
+    input_stream >> age;
+    s.set_age(age);
+
+    int attendance;
+    input_stream >> attendance;
+    s.set_attendance(attendance);
+
+    float gpa;
+    input_stream >> gpa;
+    s.set_gpa(gpa);
+
+    std::string comment;
+    input_stream >> comment;
+    s.set_comment(comment);
+
+    return input_stream;
+}
+
+bool operator<(const Student& s1, const Student& s2) {
+
+    if(s1.get_name() < s2.get_name()) return true;
+    if(s2.get_name() < s1.get_name()) return false;
+
+    if(s1.get_age() < s2.get_age()) return true;
+    if(s2.get_age() < s1.get_age()) return false;
+
+    if(s1.get_gpa() < s2.get_gpa()) return true;
+    if(s2.get_gpa() < s1.get_gpa()) return false;
+
+    return false;
+
 }
