89660bec1bd0f4dc754eac08e25b4dbd65311941
cianmcateer <cianmcateer@gmail.com>
2017-11-11 14:07:31 +0000

    Added comments

--- util.cpp
+++ util.cpp
@@ -1,5 +1,11 @@
 #include "util.h"
 
+/**
+* Returns true if 'text' matches regex validation
+* @param text
+* @param regex
+* @return bool
+*/
 bool is_correct(std::string text , const std::string regex) {
     std::regex syntax(regex);
     if(std::regex_match(text, syntax)) {
@@ -15,8 +21,8 @@ bool is_correct(std::string text , const std::string regex) {
 void read_file(const std::string path) {
     // Fix file path problem
     std::ifstream help_menu(path);
-    std::string line;
     if(help_menu.is_open()) {
+        std::string line;
         // Will read line until new line is detected
         while(std::getline(help_menu,line)) {
             std::cout << line << std::endl;
@@ -27,11 +33,20 @@ void read_file(const std::string path) {
 
 }
 
+/**
+* Returns true if argument matches password
+* @param password
+* @return bool
+*/
 bool is_password(std::string password) {
     const std::string user_password = "password";
     return user_password == password;
 }
-
+/**
+* Converts string to all upper_case
+* @param x
+* @return x
+*/
 std::string upper_case(std::string& x) {
     for(auto& c : x) c = toupper(c);
     return x;
